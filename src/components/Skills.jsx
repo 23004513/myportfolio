937fcb7e1a4abd2642cae447dceb5bede71a34ec
Navya Sree <nuthalapatinavyasree2005@gmail.com>
2026-07-06 23:47:00 +0530

    Updated portfolio skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -13,7 +13,7 @@ function Skills() {
 
         <div className="bg-gradient-to-br from-[#111827] to-[#1e1b4b] border border-purple-500/40 rounded-3xl p-10 text-center hover:-translate-y-4 hover:scale-105 transition-all duration-500 shadow-[0_0_25px_rgba(168,85,247,0.2)] hover:shadow-[0_0_50px_rgba(168,85,247,0.5)]">
           <h3 className="text-3xl font-bold text-purple-400 mb-4">
-            HTML
+            HTML5
           </h3>
           <p className="text-gray-300">
             Semantic & responsive webpage structure
@@ -22,7 +22,7 @@ function Skills() {
 
         <div className="bg-gradient-to-br from-[#111827] to-[#1e1b4b] border border-purple-500/40 rounded-3xl p-10 text-center hover:-translate-y-4 hover:scale-105 transition-all duration-500 shadow-[0_0_25px_rgba(168,85,247,0.2)] hover:shadow-[0_0_50px_rgba(168,85,247,0.5)]">
           <h3 className="text-3xl font-bold text-purple-400 mb-4">
-            CSS
+            CSS3
           </h3>
           <p className="text-gray-300">
             Modern UI styling & animations
@@ -31,7 +31,7 @@ function Skills() {
 
         <div className="bg-gradient-to-br from-[#111827] to-[#1e1b4b] border border-purple-500/40 rounded-3xl p-10 text-center hover:-translate-y-4 hover:scale-105 transition-all duration-500 shadow-[0_0_25px_rgba(168,85,247,0.2)] hover:shadow-[0_0_50px_rgba(168,85,247,0.5)]">
           <h3 className="text-3xl font-bold text-purple-400 mb-4">
-            JavaScript
+            JavaScript(ES6)
           </h3>
           <p className="text-gray-300">
             Interactive and dynamic web functionality
@@ -40,7 +40,7 @@ function Skills() {
 
         <div className="bg-gradient-to-br from-[#111827] to-[#1e1b4b] border border-purple-500/40 rounded-3xl p-10 text-center hover:-translate-y-4 hover:scale-105 transition-all duration-500 shadow-[0_0_25px_rgba(168,85,247,0.2)] hover:shadow-[0_0_50px_rgba(168,85,247,0.5)]">
           <h3 className="text-3xl font-bold text-purple-400 mb-4">
-            React
+            React.js
           </h3>
           <p className="text-gray-300">
             Building modern frontend applications
@@ -58,7 +58,7 @@ function Skills() {
 
         <div className="bg-gradient-to-br from-[#111827] to-[#1e1b4b] border border-purple-500/40 rounded-3xl p-10 text-center hover:-translate-y-4 hover:scale-105 transition-all duration-500 shadow-[0_0_25px_rgba(168,85,247,0.2)] hover:shadow-[0_0_50px_rgba(168,85,247,0.5)]">
           <h3 className="text-3xl font-bold text-purple-400 mb-4">
-            DSA
+            Data Structures & Algorithms
           </h3>
           <p className="text-gray-300">
             Problem-solving using data structures & algorithms
@@ -76,10 +76,19 @@ function Skills() {
 
         <div className="bg-gradient-to-br from-[#111827] to-[#1e1b4b] border border-purple-500/40 rounded-3xl p-10 text-center hover:-translate-y-4 hover:scale-105 transition-all duration-500 shadow-[0_0_25px_rgba(168,85,247,0.2)] hover:shadow-[0_0_50px_rgba(168,85,247,0.5)]">
   <h3 className="text-3xl font-bold text-purple-400 mb-4">
-    Tailwind CSS
+    SQL
   </h3>
   <p className="text-gray-300">
-    Building responsive and modern user interfaces
+    standard programming language used to interact with and manage relational databases
+  </p>
+</div>
+
+<div className="bg-gradient-to-br from-[#111827] to-[#1e1b4b] border border-purple-500/40 rounded-3xl p-10 text-center hover:-translate-y-4 hover:scale-105 transition-all duration-500 shadow-[0_0_25px_rgba(168,85,247,0.2)] hover:shadow-[0_0_50px_rgba(168,85,247,0.5)]">
+  <h3 className="text-3xl font-bold text-purple-400 mb-4">
+    Git & GitHub
+  </h3>
+  <p className="text-gray-300">
+    Version control, collaboration, and efficient project management using Git & GitHub.
   </p>
 </div>
 
